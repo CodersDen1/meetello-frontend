c57b1d0944e6505836fde738823dc253a312dcd1
CodersDen1 <com.github.adbhut@gmail.com>
2024-01-17 23:54:26 +0530

    Integrated backend and reducers to store auth state of the user

---
+++ src/http/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api=axios.create({
+    baseURL:process.env.REACT_APP_API,
+    headers: {
+        'Content-Type':'application/json',
+        Accept:'application/json'
+    }
+});
+
+
+//list of endpoints
+
+export const sendOTP =(data)=> api.post('/api/users/generate',data)
+
+export const verifyOtp=(data)=> api.post('/api/users/verify-otp',data)
+
+export default api;
--- src/utils/routes/ProtectedRoutes.jsx
+++ src/utils/routes/ProtectedRoutes.jsx
@@ -1,17 +1,14 @@
 
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
+import { UseSelector, useSelector } from 'react-redux';
 
-const isAuth=true;
 
-const user ={
-    name : "Adbhut Mishra",
-    usernname : "akki123",
-    activated : true
-  }
+
 
 
 const ProtectedRoutes = () => {
+  const {user,isAuth} = useSelector((state)=>state.auth)
   return (
     !isAuth ? <Navigate to='/' /> :
      isAuth && !user.activated?<Navigate to='activated'/> :<Outlet />
